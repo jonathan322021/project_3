f58d479f678371ae07da6381e05eb81cd34015d5
Jonathan Python <jonathan.amadorlorence@gmail.com>
2021-05-31 17:17:14 +0200

    starting project

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    """Add Function"""
+    return x+y
+
+def substract(X,Y):
+    """subtract function """
+    return x-y
+
+def multiply(x,y):
+    """Multiply Function"""
+    return x+y
+
+def devide(x,y):
+    """Divide function"""
+    if y == 0:
+        reaise ValueError('Can not divide by zero')
+        return x/y
